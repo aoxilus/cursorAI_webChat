8d1c1af6a13dfb5913a24c0aaf08c698fd712514
aoxilus <aoxilus@gmail.com>
2025-08-01 15:19:41 -0600

    docs: agregar script de comandos para GitHub

---
+++ github_commands.ps1
@@ -0,0 +1,7 @@
+# Comandos para conectar con GitHub
+# Reemplaza TU_USUARIO con tu nombre de usuario de GitHub
+
+git remote add origin https://github.com/TU_USUARIO/cursorAI_webChat.git
+git branch -M main
+git push -u origin main
+git push --tags
